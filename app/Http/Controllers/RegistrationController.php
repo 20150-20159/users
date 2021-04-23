16674a3ff5e61eb9eee3dd479b6284b334cf3301
Dimitris Frangiadakis <itp20159@hua.gr>
2021-04-23 21:33:02 +0300

    Adds user registration

---
+++ app/Http/Controllers/RegistrationController.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\User;
+use Illuminate\Http\Request;
+
+class RegistrationController extends Controller
+{
+    public function register(Request $request)
+    {
+        $existingUser = User::where('email', $request->get('email'))->first();
+
+        if (!empty($existingUser))
+        {
+            return response('User already exists', 409);
+        }
+
+        User::create([
+            'name' => $request->get('name'),
+            'email' => $request->get('email'),
+            'password' => password_hash($request->get('password'), PASSWORD_DEFAULT),
+            'vat' => $request->get('vat')
+        ]);
+
+        return response('User created', 201);
+    }
+}
