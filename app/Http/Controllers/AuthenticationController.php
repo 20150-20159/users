1b442603f7074885dbc57121f1bbd29b2fd7cb60
Dimitris Frangiadakis <itp20159@hua.gr>
2021-04-23 21:59:48 +0300

    Authenticates users

--- app/Http/Controllers/AuthenticationController.php
+++ app/Http/Controllers/AuthenticationController.php
@@ -3,11 +3,27 @@
 namespace App\Http\Controllers;
 
 use App\Models\User;
+use Illuminate\Http\Request;
 
 class AuthenticationController extends Controller
 {
     public function getUser(User $user): User
     {
         return $user;
+    }
+
+    public function authenticateUser(Request $request)
+    {
+        $user = User::where('email', $request->get('email'))->first();
+
+        if (!empty($user))
+        {
+            if (!password_verify($request->get('password'), $user->password))
+            {
+                return response('User/password combination not found', 401);
+            }
+        }
+
+        return response($user);
     }
 }
--- routes/web.php
+++ routes/web.php
@@ -17,3 +17,4 @@ use Illuminate\Support\Facades\Route;
 
 Route::post('/register', [RegistrationController::class, 'register']);
 Route::get('/user/{user}', [AuthenticationController::class, 'getUser']);
+Route::post('/user/authenticate', [AuthenticationController::class, 'authenticateUser']);
