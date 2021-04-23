ad68f26dfed6640694598bd2d614dd258158ee87
Dimitris Frangiadakis <itp20159@hua.gr>
2021-04-23 21:47:43 +0300

    Retrieves users

--- app/Http/Controllers/AuthenticationController.php
+++ app/Http/Controllers/AuthenticationController.php
@@ -2,9 +2,12 @@
 
 namespace App\Http\Controllers;
 
-use Illuminate\Http\Request;
+use App\Models\User;
 
 class AuthenticationController extends Controller
 {
-    //
+    public function getUser(User $user): User
+    {
+        return $user;
+    }
 }
--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\AuthenticationController;
 use App\Http\Controllers\RegistrationController;
 use Illuminate\Support\Facades\Route;
 
@@ -15,3 +16,4 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::post('/register', [RegistrationController::class, 'register']);
+Route::get('/user/{user}', [AuthenticationController::class, 'getUser']);
